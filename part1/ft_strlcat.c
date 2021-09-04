be416ff731f31c26f654176a0e98e9d0764c3e40
darylgaw <darylgaw97@gmail.com>
2021-09-04 11:14:13 +0000

    strlcat

---
+++ part1/ft_strlcat.c
@@ -0,0 +1,32 @@
+
+static int ft_strlen(const char *str);
+
+size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+    int i;
+    int j;
+    int return_value;
+
+    if (dstsize == 0)
+        return (0);
+    return_value = ft_strlen(dst) + ft_strlen(src);
+    i = ft_strlen(dst);
+    j = 0;
+    while (src[j] && (long unsigned)(i + j) < dstsize - 1)
+    {
+        dst[i + j] = src[j];
+        j++;
+    }
+    dst[i + j] = '\0';
+    return (return_value);
+}
+
+static int ft_strlen(const char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
