7ae02dd61bec02b30a163c511978322764d64333
darylgaw <darylgaw97@gmail.com>
2021-09-05 13:10:00 +0000

    memcpy added comment

--- part1/ft_memcpy.c
+++ part1/ft_memcpy.c
@@ -14,3 +14,11 @@ void *ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
     }
     return (typecasted_dest);
 }
+
+/*
+    Typecasting dest and src into type "char *" to facilitate variable assignment.
+
+    When copying type "int" make sure to input correct "size" as a bigger integer may require more memory,
+    typically just use the "sizeof" operator to prevent allocating the wrong size for whatever variable type that is being copied.
+    For example: sizeof(int) == 4 bytes, sizeof(char) == 1 byte
+/*
