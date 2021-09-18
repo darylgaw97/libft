3f40f9434bbea8b4d5f5fe43a6789612bb002ad8
darylgaw <darylgaw97@gmail.com>
2021-09-18 15:45:47 +0000

    strmapi

--- part1/ft_strchr.c
+++ part1/ft_strchr.c
@@ -3,7 +3,7 @@ char *ft_strchr(char *str, char c)
     // checking if char matches c or already reached th eend of string
     while (*str != c && *str != '\0')
         str++;
-    // functions returns '\0' if c is specified as '\0'
+    // functions returns address '\0' if c is specified as '\0'
     // if c is not found then NULL is returned instead
     if (c != '\0')
         return (NULL);
---
+++ part2/ft_strmapi.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+static int ft_strlen(char const *str);
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char *new_str;
+    int i;
+
+    if (s == NULL || f == NULL)     //checking if 's' or 'f' is NULL, failure to do so can result in seg fault when trying to dereference 's' or 'f'
+        return (NULL);
+    if (!(new_str = malloc(ft_strlen(s) + 1)))
+        return (NULL);
+    i = 0;
+    while (s[i])
+    {
+        new_str[i] = f(i, s[i]);    //modifying individual characters in s using function pointed by f, then assigning them to "new_str"
+        i++;
+    }
+    new_str[i] = '\0';
+    return (new_str);
+}
+
+static int ft_strlen(char const *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
