ab7daf3e04a5be02706c8ac8053f1a3a0d660c77
darylgaw <darylgaw97@gmail.com>
2021-09-21 04:29:40 +0000

    lstnew lstadd_front

--- bonus_part/testers/lstadd_front_main.c
+++ bonus_part/testers/lstadd_front_main.c
@@ -14,35 +14,36 @@ int main(void)
 {
     t_list *list;
     t_list *alpha;
-    t_list *n;
-    
+    t_list *n = NULL;
+
     list = malloc(sizeof(t_list));
     if (list == NULL)
         return (1);
     alpha = list;
-    n = malloc(sizeof(t_list));
+    /*n = malloc(sizeof(t_list));
     if (n == NULL)
         return (1);
-    
+    */
     for (int  i = 0; i < 10; i++)
     {
         alpha->c = 'a' + i;
         alpha->next = malloc(sizeof(t_list));
         alpha = alpha->next;
     }
-    
+
     printf("Before:\n");
     for (alpha = list; alpha->next; alpha = alpha->next)
     {
         printf("%c\n", alpha->c);
     }
-    
-    n->c = 'z';
+
+    //n->c = 'z';
     ft_lstadd_front(&list, n);
-    
+
     printf("\nAfter:\n");
     for (alpha = list; alpha->next; alpha = alpha->next)
     {
         printf("%c\n", alpha->c);
     }
 }
+
