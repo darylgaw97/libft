302fc267e6893fb710bcd33e57ba33fd9d5287f0
darylgaw <darylgaw97@gmail.com>
2021-10-04 06:36:01 +0000

    edit bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -3,11 +3,13 @@
 void ft_bzero(void *s, size_t n)
 {
     size_t i;
+    char *mem;
 
+    mem = s;
     i = 0;
     while (i < n)
     {
-        s[i] = '\0';
+        mem[i] = 0;
         i++;
     }
 }
