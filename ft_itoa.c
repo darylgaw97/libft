e34d5c5d4cfd029ac475e7697c861e0b32866a93
darylgaw <darylgaw97@gmail.com>
2021-10-06 07:03:39 +0000

    itoa pre-test

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,54 +1,68 @@
 #include "libft.h"
 
-static void is_negative(char *str, long *num, int *i);
+static char *int_min(void);
+static int numstrlen(int n);
+static char *make_a(char *a, int n);
 
 char *ft_itoa(int n)
 {
-    char *str;
-    long num;
-    long divider;
+    char *a;
+
+    if (n == INT_MIN)
+        return (int_min());
+    if (!(a = malloc(numstrlen(n)) + 1))
+        return (NULL);
+    return (make_a(a, n));
+}
+
+static char *make_a(char *a, int n)
+{
     int i;
 
-    str = malloc(12);   //malloc-ed 12 bytes (max int -2147483648 is 11 chars long, +1 byte for null cap)
-    if (str == NULL)
-        return (NULL);
-    num = n;
     i = 0;
-    if (num < 0)        //checking for negative integers, if yes, first char becomes '-', then num is converted to positive integer
-        is_negative(str, &num, &i);
-    else if (num == 0)  //this is an edge case (dividing zero by zero will result in error)
-        return ("0");
-    divider = 1;
-    while (num / divider != 0)  //setting 'divider' to the correct size, according to the size of num
-        divider *= 10;
-    //start assigning to str each digit, decreasing 'divider' by one zero after each iteration, so that the next digit can be obtained,
-    //when 'divider' becomes '0', means that the last digit has been converted to char.
-    while ((divider /= 10) != 0)
+    if (n < 0)
     {
-        str[i] = (num % (divider * 10) / divider) + '0';
+        a[i] = '-';
+        n *= -1;
+    }
+    i = numstrlen(n);
+    a[i] = '\0';
+    i--;
+    if (n == 0)
+        a[i] = '0';
+    while (n != 0)
+    {
+        a[i] = (n % 10) + '0';
+        i--;
+        n = n / 10;
+    }
+    return (a);
+}
+
+static char *int_min(void)
+{
+    char *a;
+
+    if (!(a = malloc(12)))
+        return (NULL);
+    a = "-2147483648";
+    return (a);
+}
+
+static int numstrlen(int n)
+{
+    int i;
+
+    i = 1;
+    if (n < 0)
+    {
+        n *= -1;
         i++;
     }
-    str[i] = '\0';  //remember to null cap
-    return (str);
+    while (n / 10 != 0)
+    {
+        n /= 10;
+        i++;
+    }
+    return (i);
 }
-
-static void is_negative(char *str, long *num, int *i)
-{
-    str[0] = '-';
-    *num /= -1;
-    *i = 1;
-}
-
-/*
-    Somewhat confusing syntax as norminette only allows 25 lines per function, a lot of compression was done in the code above,
-    hence the reduction in readiblity.
-    An alternative is to use helper functions but it could make the code even harder to read if not written well.
-    In this case, the "is_negative" function is used to save 4 lines.
-
-    code tested for:
-        1.             -2147483648
-        2.              2147483647
-        3.                       0
-        4. random positive integer
-        5. random negative integer
-*/
