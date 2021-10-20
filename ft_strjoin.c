64a458e27e04c3625d406e71c06dad0fc44fc703
darylgaw <darylgaw97@gmail.com>
2021-10-20 12:39:04 +0000

    optimized ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -2,9 +2,8 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		i;
-	int		j;
 	char	*joined;
+	size_t	i;
 
 	if (s1 == NULL)
 		return (ft_strdup(s2));
@@ -14,17 +13,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (joined == NULL)
 		return (NULL);
 	i = 0;
-	while (s1[i])
-	{
-		joined[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		joined[i + j] = s2[j];
-		j++;
-	}
-	joined[i + j] = '\0';
+	while (*s1)
+		joined[i++] = *s1++;
+	while (*s2)
+		joined[i++] = *s2++;
+	joined[i] = '\0';
 	return (joined);
 }
