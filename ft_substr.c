c9c824cecaf51974ac4c6b3f63912200015de24f
darylgaw <darylgaw97@gmail.com>
2021-10-20 02:07:38 +0000

    optimized ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -7,14 +7,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (s == NULL)
 		return (NULL);
+	if (start >= (unsigned)ft_strlen(s))
+	    return (ft_strdup(""));
 	if (len < (unsigned)ft_strlen(s))
 		substr = malloc(len + 1);
 	else
 		substr = malloc(ft_strlen(s) + 1);
 	if (substr == NULL)
 		return (NULL);
-	if (start >= (unsigned)ft_strlen(s))
-	    return (ft_strdup(""));
 	i = 0;
 	while (s[i] && i < len)
 	{
